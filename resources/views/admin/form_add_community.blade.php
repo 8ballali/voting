dcbb4bca7d1350e76ce206f64c4fa31a622f9699
8ballali <iqbalalisaidil@outlook.com>
2021-06-17 10:12:29 +0700

    Merapikan

--- resources/views/admin/form_add_community.blade.php
+++ resources/views/admin/form_add_community.blade.php
@@ -1,80 +1,83 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
-        <title>CRUD Eloquent Laravel</title>
-    </head>
-    <body>
-        <div class="container">
-            <div class="card mt-5">
-                <div class="card-header text-center">
-                    Add Community
-                </div>
-                <div class="card-body">
-                    <a href="/e-vote/admin/community" class="btn btn-primary">Back</a>
-                    <br/>
-                    <br/>
-                    
-                    <form method="post" action="/e-vote/admin/community/store">
 
-                        {{ csrf_field() }}
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
+    <title>CRUD Eloquent Laravel</title>
+</head>
 
-                        <div class="form-group">
-                            <label>Community Name</label>
-                            <input type="text" name="name" class="form-control" placeholder="Nama Komunitas">
+<body>
+    <div class="container">
+        <div class="card mt-5">
+            <div class="card-header text-center">
+                Add Community
+            </div>
+            <div class="card-body">
+                <a href="/e-vote/admin/community" class="btn btn-primary">Back</a>
+                <br />
+                <br />
 
-                            @if($errors->has('name'))
-                                <div class="text-danger">
-                                    {{ $errors->first('name')}}
-                                </div>
-                            @endif
+                <form method="post" action="/e-vote/admin/community/store">
 
+                    {{ csrf_field() }}
+
+                    <div class="form-group">
+                        <label>Community Name</label>
+                        <input type="text" name="name" class="form-control" placeholder="Nama Komunitas">
+
+                        @if($errors->has('name'))
+                        <div class="text-danger">
+                            {{ $errors->first('name')}}
                         </div>
+                        @endif
 
-                        <div class="form-group">
-                            <label>Description</label>
-                            <textarea name="description" class="form-control" placeholder="Deskripsi Komunitas"></textarea>
+                    </div>
 
-                             @if($errors->has('description'))
-                                <div class="text-danger">
-                                    {{ $errors->first('description')}}
-                                </div>
-                            @endif
+                    <div class="form-group">
+                        <label>Description</label>
+                        <textarea name="description" class="form-control" placeholder="Deskripsi Komunitas"></textarea>
 
+                        @if($errors->has('description'))
+                        <div class="text-danger">
+                            {{ $errors->first('description')}}
                         </div>
-                        <div class="form-group">
-                            <label>Description</label>
-                            <textarea name="description" class="form-control" placeholder="Deskripsi Komunitas"></textarea>
+                        @endif
 
-                             @if($errors->has('description'))
-                                <div class="text-danger">
-                                    {{ $errors->first('description')}}
-                                </div>
-                            @endif
+                    </div>
+                    <div class="form-group">
+                        <label>Description</label>
+                        <textarea name="description" class="form-control" placeholder="Deskripsi Komunitas"></textarea>
 
+                        @if($errors->has('description'))
+                        <div class="text-danger">
+                            {{ $errors->first('description')}}
                         </div>
+                        @endif
 
-                        <div class="form-group">
-                            <label>Phone Number</label>
-                            <textarea name="phone" class="form-control" placeholder="Nomor Telepon"></textarea>
+                    </div>
 
-                             @if($errors->has('phone'))
-                                <div class="text-danger">
-                                    {{ $errors->first('phone')}}
-                                </div>
-                            @endif
+                    <div class="form-group">
+                        <label>Phone Number</label>
+                        <textarea name="phone" class="form-control" placeholder="Nomor Telepon"></textarea>
 
+                        @if($errors->has('phone'))
+                        <div class="text-danger">
+                            {{ $errors->first('phone')}}
                         </div>
-                        <div class="form-group">
-                            <input type="submit" class="btn btn-success" value="Simpan">
-                        </div>
+                        @endif
 
-                    </form>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-success" value="Simpan">
+                    </div>
 
-                </div>
+                </form>
+
             </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
--- resources/views/users/candidate.blade.php
+++ resources/views/users/candidate.blade.php
@@ -1,58 +1,62 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Data Community</title>
-	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <title>Data Community</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
 </head>
+
 <body>
 
-	<div class="container">
+    <div class="container">
 
-		<div class="card mt-5">
-			<div class="card-header text-center">
-				Data Kandidat 
-			</div>
-			<div class="card-body center">
-				<br/>
-				<br/>
+        <div class="card mt-5">
+            <div class="card-header text-center">
+                Data Kandidat
+            </div>
+            <div class="card-body center">
+                <br />
+                <br />
 
-				<table class="table table-bordered">
-					<thead>
-						<tr>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
                             <th>ID</th>
-							<th>Nama Kandidat</th>
-							<th>Visi Kandidat</th>
-							<th>Misi Kandidat</th>
+                            <th>Nama Kandidat</th>
+                            <th>Visi Kandidat</th>
+                            <th>Misi Kandidat</th>
                             <th>Alamat </th>
                             <th>TTL</th>
                             <th>Gender</th>
                             <th>User_phone</th>
                             <th>User_id</th>
                             <th>file Syarat</th>
-					</thead>
-					<tbody>
-						@foreach($candidate as $ca)
-						<tr>
-							<td>{{ $ca->id }}</td>
-							<td>{{ $ca->name }}</td>
-							<td>{{ $ca->visi }}</td>
-							<td>{{ $ca->misi }}</td>
-							<td>{{ $ca->alamat }}</td>
-							<td>{{ $ca->ttl }}</td>
-							<td>{{ $ca->gender }}</td>
-							<td>{{ $ca->user_phone }}</td>
-							<td>{{ $ca->user_id }}</td>
-							<td>{{ $ca->file }}</td>
-							
-						</tr>
-						@endforeach
-						
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</div>
+                    </thead>
+                    <tbody>
+                        @foreach($candidate as $ca)
+                        <tr>
+                            <td>{{ $ca->id }}</td>
+                            <td>{{ $ca->name }}</td>
+                            <td>{{ $ca->visi }}</td>
+                            <td>{{ $ca->misi }}</td>
+                            <td>{{ $ca->alamat }}</td>
+                            <td>{{ $ca->ttl }}</td>
+                            <td>{{ $ca->gender }}</td>
+                            <td>{{ $ca->user_phone }}</td>
+                            <td>{{ $ca->user_id }}</td>
+                            <td>{{ $ca->file }}</td>
+
+                        </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
 </body>
+
 </html>
