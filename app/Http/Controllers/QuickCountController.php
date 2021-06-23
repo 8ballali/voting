b5b71380ecaee9b9587428a1cb04da1b18222848
8ballali <iqbalalisaidil@outlook.com>
2021-06-23 22:46:36 +0700

    Done Project

--- app/Http/Controllers/QuickCountController.php
+++ app/Http/Controllers/QuickCountController.php
@@ -9,9 +9,11 @@ use Illuminate\Support\Facades\DB;
 class QuickCountController extends Controller
 {
     public function index (){
+
              $label = DB::table('vote')->selectRaw(DB::raw('count(*) as name'))->groupBy('candidate_id')->orderBy('candidate_id', 'asc')->get()->map(function($x) {
             return $x->name;
             });
+
             $data = DB::table('candidate')->join('vote', 'vote.candidate_id', 'candidate.id')->select('candidate.name')->orderBy('candidate.id', 'asc')->get(
             )->map(function($x) {
             return $x->name;
