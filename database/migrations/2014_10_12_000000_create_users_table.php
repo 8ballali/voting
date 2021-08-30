1be824ef5a8fc65a3c7a3830f95eee7d236bd1bc
8ballali <iqbalalisaidil@outlook.com>
2021-08-30 11:11:17 +0700

    remove unique email user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->text('phone');
             $table->unsignedBigInteger('community_id');
             $table->string('role')->nullabel();
