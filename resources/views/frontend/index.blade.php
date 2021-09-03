8476c41871f95d6d6bed4d09850022d942e581cf
unknown <restuyogasmk7@gmail.com>
2021-09-03 13:32:10 +0700

    countdown lagi

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -109,112 +109,8 @@
         <div class="section-title">
             <h2 class="">{{$p->title}}</h2>
             <div class="countdown">
-                <!-- Days -->
-                <div class="bloc-time days" data-init-value="7">
-                    <span class="count-title">Days</span>
-
-                    <div class="figure days days-1">
-                        <span class="top">0</span>
-                        <span class="top-back">
-                            <span>0</span>
-                        </span>
-                        <span class="bottom">0</span>
-                        <span class="bottom-back">
-                            <span>0</span>
-                        </span>
-                    </div>
-
-                    <div class="figure days days-2">
-                        <span class="top">0</span>
-                        <span class="top-back">
-                            <span>0</span>
-                        </span>
-                        <span class="bottom">0</span>
-                        <span class="bottom-back">
-                            <span>0</span>
-                        </span>
-                    </div>
-                </div>
-                <!-- Hours -->
-                <div class="bloc-time hours" data-init-value="24">
-                    <span class="count-title">Hours</span>
-
-                    <div class="figure hours hours-1">
-                        <span class="top">0</span>
-                        <span class="top-back">
-                            <span>0</span>
-                        </span>
-                        <span class="bottom">0</span>
-                        <span class="bottom-back">
-                            <span>0</span>
-                        </span>
-                    </div>
-
-                    <div class="figure hours hours-2">
-                        <span class="top">0</span>
-                        <span class="top-back">
-                            <span>0</span>
-                        </span>
-                        <span class="bottom">0</span>
-                        <span class="bottom-back">
-                            <span>0</span>
-                        </span>
-                    </div>
-                </div>
-                <!-- Minutes -->
-                <div class="bloc-time min" data-init-value="0">
-                    <span class="count-title">Minutes</span>
-
-                    <div class="figure min min-1">
-                        <span class="top">0</span>
-                        <span class="top-back">
-                            <span>0</span>
-                        </span>
-                        <span class="bottom">0</span>
-                        <span class="bottom-back">
-                            <span>0</span>
-                        </span>
-                    </div>
-
-                    <div class="figure min min-2">
-                        <span class="top">0</span>
-                        <span class="top-back">
-                            <span>0</span>
-                        </span>
-                        <span class="bottom">0</span>
-                        <span class="bottom-back">
-                            <span>0</span>
-                        </span>
-                    </div>
-                </div>
-                <!-- Second -->
-                <div class="bloc-time sec" data-init-value="0">
-                    <span class="count-title">Seconds</span>
-
-                    <div class="figure sec sec-1">
-                        <span class="top">0</span>
-                        <span class="top-back">
-                            <span>0</span>
-                        </span>
-                        <span class="bottom">0</span>
-                        <span class="bottom-back">
-                            <span>0</span>
-                        </span>
-                    </div>
-
-                    <div class="figure sec sec-2">
-                        <span class="top">0</span>
-                        <span class="top-back">
-                            <span>0</span>
-                        </span>
-                        <span class="bottom">0</span>
-                        <span class="bottom-back">
-                            <span>0</span>
-                        </span>
-                    </div>
-                </div>
+                <div id="countdown"></div>
             </div>
-        </div>
     </section>
     <section style="margin-top: -80px">
         <div class="slider owl-carousel">
@@ -286,179 +182,40 @@
 
     @foreach ($poling as $p)
     <script>
-        var Countdown = {
+        // set the date we're counting down to
+        var target_date = new Date(0).getTime();
 
-            // Backbone-like structure
-            $el: $('.countdown'),
+        // variables for time units
+        var days, hours, minutes, seconds;
 
-            // Params
-            countdown_interval: null,
-            total_seconds: 0,
+        // get tag element
+        var countdown = document.getElementById('countdown');
 
-            // Initialize the countdown  
-            init: function () {
-                var Today = new Date();
-                var Todayday = Today
-                    .getDate(); // getDay() is an integer corresponding to the day of the week: 0 for Sunday, 1 for Monday, 2 for Tuesday, and so on.
-                var Todayhours = Today.getHours();
-                var Todayminutes = Today.getMinutes();
-                var Todayseconds = Today.getSeconds();
+        // update the tag with id "countdown" every 1 second
+        setInterval(function () {
 
-                var DeadLine = new Date();
-                var DeadLineday = DeadLine.getDate();
-                var DeadLinehours = DeadLine.getHours(); // I'm not sure why getHours() here is "loaded jangá"
-                var DeadLineminutes = DeadLine.getMinutes();
-                var DeadLineseconds = DeadLine.getSeconds();
+            // find the amount of "seconds" between now and target
+            var current_date = new Date(0).getTime();
+            var seconds_left = (target_date - current_date) / 1000;
 
-                // DOM
-                this.$ = {
-                    days: this.$el.find('.bloc-time.days .figure'),
-                    hours: this.$el.find('.bloc-time.hours .figure'),
-                    minutes: this.$el.find('.bloc-time.min .figure'),
-                    seconds: this.$el.find('.bloc-time.sec .figure')
-                };
+            // do some time calculations
+            days = parseInt(seconds_left / 86400);
+            seconds_left = seconds_left % 86400;
 
-                // Init countdown values
-                this.values = {
-                    days: DeadLineday - Todayday,
-                    hours: DeadLinehours - Todayhours,
-                    minutes: DeadLineminutes - Todayminutes,
-                    seconds: DeadLineseconds - Todayseconds
-                };
+            hours = parseInt(seconds_left / 3600);
+            seconds_left = seconds_left % 3600;
 
-                // Initialize total seconds
-                // this.total_seconds = ((this.values.days * 24) + (this.values.hours * 60 * 60 + (this.values.minutes * 60))) + this.values.seconds;
-                this.total_seconds = this.values.seconds + (this.values.minutes * 60) + (this.values.hours *
-                    60 * 60) + (this.values.days * 24 * 60 * 60);
+            minutes = parseInt(seconds_left / 60);
+            seconds = parseInt(seconds_left % 60);
 
-                // Animate countdown to the end 
-                this.count();
-            },
+            // format countdown string + set tag value
+            countdown.innerHTML = '<span class="days">' + days +
+                ' <label>Days</label></span> <span class="hours">' + hours +
+                ' <label>Hours</label></span> <span class="minutes">' +
+                minutes + ' <label>Minutes</label></span> <span class="seconds">' + seconds +
+                ' <label>Seconds</label></span>';
 
-            count: function () {
-
-                var that = this,
-                    $day_1 = this.$.days.eq(0),
-                    $day_2 = this.$.days.eq(1),
-                    $hour_1 = this.$.hours.eq(0),
-                    $hour_2 = this.$.hours.eq(1),
-                    $min_1 = this.$.minutes.eq(0),
-                    $min_2 = this.$.minutes.eq(1),
-                    $sec_1 = this.$.seconds.eq(0),
-                    $sec_2 = this.$.seconds.eq(1);
-
-                this.countdown_interval = setInterval(function () {
-
-                    if (that.total_seconds > 0) {
-
-                        --that.values.seconds;
-
-                        if (that.values.minutes >= 0 && that.values.seconds <= 0) {
-
-                            that.values.seconds = 59;
-                            --that.values.minutes;
-                        } // end if
-
-                        if (that.values.hours >= 0 && that.values.minutes <= 0) {
-
-                            that.values.minutes = 59;
-                            --that.values.hours;
-                        } // end if
-
-                        if (that.values.days >= 0 && that.values.hours <= 0) {
-
-                            that.values.hours = 24;
-                            --that.values.days;
-                        } // end if
-
-                        if (that.values.days <= 0 && that.values.hours >= 0) {
-
-                        that.values.hours = 0;
-                        that.values.days= 0;
-                        }
-
-                        // Update DOM values
-                        // Days
-                        that.checkDay(that.values.days, $day_1, $day_2);
-
-                        // Hours
-                        that.checkDay(that.values.hours, $hour_1, $hour_2);
-
-                        // Minutes
-                        that.checkDay(that.values.minutes, $min_1, $min_2);
-
-                        // Seconds
-                        that.checkDay(that.values.seconds, $sec_1, $sec_2);
-
-                        --that.total_seconds;
-                    } // end if(that.total_seconds > 0)
-                    else {
-                        clearInterval(that.countdown_interval);
-                    }
-                }, 1000);
-            },
-
-            animateFigure: function ($el, value) {
-
-                var that = this,
-                    $top = $el.find('.top'),
-                    $bottom = $el.find('.bottom'),
-                    $back_top = $el.find('.top-back'),
-                    $back_bottom = $el.find('.bottom-back');
-
-                // Before we begin, change the back value
-                $back_top.find('span').html(value);
-
-                // Also change the back bottom value
-                $back_bottom.find('span').html(value);
-
-                // Then animate
-                TweenMax.to($top, 0.8, {
-                    rotationX: '-180deg',
-                    transformPerspective: 300,
-                    ease: Quart.easeOut,
-                    onComplete: function () {
-
-                        $top.html(value);
-
-                        $bottom.html(value);
-
-                        TweenMax.set($top, {
-                            rotationX: 0
-                        });
-                    }
-                });
-
-                TweenMax.to($back_top, 0.8, {
-                    rotationX: 0,
-                    transformPerspective: 300,
-                    ease: Quart.easeOut,
-                    clearProps: 'all'
-                });
-            },
-
-            checkDay: function (value, $el_1, $el_2) {
-
-                var val_1 = value.toString().charAt(0),
-                    val_2 = value.toString().charAt(1),
-                    fig_1_value = $el_1.find('.top').html(),
-                    fig_2_value = $el_2.find('.top').html();
-
-                if (value >= 10) {
-
-                    // Animate only if the figure has changed
-                    if (fig_1_value !== val_1) this.animateFigure($el_1, val_1);
-                    if (fig_2_value !== val_2) this.animateFigure($el_2, val_2);
-                } else {
-
-                    // If we are under 10, replace first figure with 0
-                    if (fig_1_value !== '0') this.animateFigure($el_1, 0);
-                    if (fig_2_value !== val_1) this.animateFigure($el_2, val_1);
-                }
-            }
-        };
-        // Let's go !
-        Countdown.init();
+        }, 1000);
 
     </script>
     @endforeach
